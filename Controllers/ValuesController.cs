92806134407ced89f2bdde328524b4d9372a9ba2
Datunashvili Giorgi <datunashvili_giorgi@yahoo.com>
2020-12-09 01:33:04 +0400

    Add project files.

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MijnuriAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MijnuriAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext dataContext;
+
+        public ValuesController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await dataContext.Values.ToListAsync();
+
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await dataContext.Values.FirstOrDefaultAsync(x => x.Id == id);
+
+            return Ok(value);
+        }
+
+        [HttpGet("strings")]
+        public ActionResult<IEnumerable<string>> GetStringArray()
+        {
+            return new string[] { "ერთი", "ორი" };
+        }
+
+        [HttpGet("strings/{id}")]
+        public ActionResult<string> GetSingleString(int id)
+        {
+            return "მხოლოდ ერთი სტრინგი";
+        }
+    }
+}
